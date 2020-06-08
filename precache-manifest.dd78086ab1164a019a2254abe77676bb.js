80fe159f120f9ed0839db82f1cbaeb84da140fba
manish-l-au3 <slmmadhavan@gmail.com>
2020-06-08 21:28:40 +0530

    Updates

---
+++ precache-manifest.dd78086ab1164a019a2254abe77676bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8addda7d864306f4cd1a70c53ff6b73b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "63e0465c3803c16ee95c",
+    "url": "/monsters-rolodex/static/css/main.cba9b712.chunk.css"
+  },
+  {
+    "revision": "92aa68b823d7f50b4a00",
+    "url": "/monsters-rolodex/static/js/2.7c78d307.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7c78d307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e0465c3803c16ee95c",
+    "url": "/monsters-rolodex/static/js/main.c01877ed.chunk.js"
+  },
+  {
+    "revision": "6ddbb5876f551689eb83",
+    "url": "/monsters-rolodex/static/js/runtime-main.98e5dc0a.js"
+  }
+]);
